5c8dc001779025527509da0ab96650ec69154fd8
lironma1 <kaskasof1@gmail.com>
2018-06-19 11:22:08 +0300

    go

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile : Profile 
+    {
+        public MappingProfile()
+        {
+            //Domain to Dot --customer
+            Mapper.CreateMap<Customer, CustomerDto>();
+            //Dot to Domain --customer
+            Mapper.CreateMap<CustomerDto, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+
+            //Domain to Dot --movie
+            Mapper.CreateMap<Movie, MovieDto>();
+            //Dot to Domain --movie
+            Mapper.CreateMap<MovieDto, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
+
+            //Domain to Dot --MembershipType
+            Mapper.CreateMap<MembershipType, MembershipTypeDto>();
+
+            //Domain to Dot --Genre
+            Mapper.CreateMap<Genre, GenreDto>();
+
+        }
+    }
+}
